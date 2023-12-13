5747b61b0d25fce26395b4e4a5f825f44b2d3f7d
Kevin Campos <kesc23@hotmail.com>
2023-12-13 20:03:07 -0300

    Create +page.svelte
    the (old) main page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    import '$lib/styles.css';
+</script>
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@800&family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <title>Kevin Campos</title>
+    <script type="application/ld+json">{ "@context": "https://schema.org/", "@type": "ProfilePage", "mainEntity": { "@type": "Person", "name": "Kevin Campos", "url": "https://kevincampos.com.br", "image": "https://kevincampos.com.br/kevin.png", "sameAs": [ "https://www.linkedin.com/in/kevinsouzacampos/", "https://github.com/kesc23/" ], "jobTitle": "Communications Analyst", "worksFor": { "@type": "Organization",   "name": "Teknisa" } } }</script>
+</svelte:head>
+
+<div style="display: flex; flex-direction: column; justify-content: center; position: fixed; width: 100%; top: 0; background-color: white; box-shadow: 0px -95px 130px;">
+    <div style="margin-inline: min(5%, 185px); display: flex; flex-wrap: wrap; max-height: 100%; max-width: 1200px; width: 100%;margin-inline: auto;" id="nav">
+        <h1>Kevin Campos (Kesc23)</h1>
+        <h2>Site in Construction</h2>
+        <nav>
+            <ul>
+                <li><a href="#projetos">Projetos</a></li>
+                <li><a href="#sobremim">Sobre mim</a></li>
+                <li><a href="#servicos">Serviços</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="https://github.com/kesc23/">Github</a></li>
+                <li><a href="https://linkedin.com/in/kevinsouzacampos/">LinkedIn</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+<div id="cont">
+    <div style="max-width: 1200px; margin-inline: auto;">
+        <div id="projetos" class="section">
+            <h2>Projetos</h2>
+            <br><br>
+            <br><br>
+            <br><br>
+        </div>
+        <div id="sobremim" class="section">
+            <h2>Sobre mim</h2>
+        </div>
+        <div id="servicos" class="section">
+            <h2>Serviços</h2>
+        </div>
+    </div>
+</div>
