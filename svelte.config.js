7152e9f446ca11a61a4fabbb4dbbb27df81700ff
Kevin Campos <kesc23@hotmail.com>
2023-12-13 20:20:03 -0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -10,10 +10,7 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	}
 };
 
