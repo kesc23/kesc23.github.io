0d81a96841e93f1d8fbc00bf89d7f3bf23a9a243
Kevin Campos <kesc23@hotmail.com>
2023-12-13 21:33:50 -0300

    added tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,8 @@ const config = {
 			assets: 'build',
 			fallback: '404.html'
 		})
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
